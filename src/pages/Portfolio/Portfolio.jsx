eeeeee931ee91de80b8f9dfc4aada72dad464e3d
YSergo <102493873+YSergo@users.noreply.github.com>
2023-10-04 15:40:35 +0400

    Refactor renderSkeletons() in portfolio page

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -10,85 +10,51 @@ function Portfolio({ projects, projectsLoading, isMobile, isTablet }) {
     window.scrollTo(0, 0);
   }, []);
 
-  const renderSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 4; i++) {
-      skeletons.push(
+  const renderSkeletons = (count, width, height) => {
+    return Array(count)
+      .fill()
+      .map((_, i) => (
         <ContentLoader
           key={i}
           speed={2}
-          width={443}
-          height={554}
-          viewBox='0 0 443 554'
-          backgroundColor='#f3f3f3'
-          foregroundColor='white'
-        >
-          <rect x='0' y='0' rx='0' ry='0' width='443' height='554' />
-        </ContentLoader>
-      );
-    }
-    return skeletons;
-  };
-
-  const renderMobileSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 8; i++) {
-      skeletons.push(
-        <ContentLoader
-          key={i}
-          speed={2}
-          width='22.5rem'
-          height='31.4rem'
+          width={width}
+          height={height}
           viewBox='0 0 443 554'
           backgroundColor='#f3f3f3'
           foregroundColor='white'
         >
           <rect x='0' y='0' rx='0' ry='0' width='100%' height='100%' />
         </ContentLoader>
-      );
-    }
-    return skeletons;
+      ));
   };
 
-  const renderTabletSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 4; i++) {
-      skeletons.push(
-        <ContentLoader
-          key={i}
-          speed={2}
-          width='calc(50% - 2.4vw)'
-          height='54vw'
-          viewBox='0 0 443 554'
-          backgroundColor='#f3f3f3' //'
-          foregroundColor='white'
-        >
-          <rect x='15' y='10' rx='0' ry='0' width='93%' height='93%' />
-        </ContentLoader>
-      );
+  let content;
+  if (projectsLoading) {
+    switch (true) {
+      case isMobile:
+        content = renderSkeletons(8, '22.5rem', '31.4rem');
+        break;
+      case isTablet:
+        content = renderSkeletons(4, 'calc(50% - 2.4vw)', '54vw');
+        break;
+      default:
+        content = renderSkeletons(4, 443, 554);
     }
-    return skeletons;
-  };
+  } else {
+    content = projects.map((item) => (
+      <PortfolioCard
+        key={item.id}
+        title={item.title}
+        description={item.description}
+        image={item.image_url}
+        url={item.url_to_project}
+      />
+    ));
+  }
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.content}>
-        {projectsLoading
-          ? isMobile
-            ? renderMobileSkeletons()
-            : isTablet
-            ? renderTabletSkeletons()
-            : renderSkeletons()
-          : projects.map((item) => (
-              <PortfolioCard
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                image={item.image_url}
-                url={item.url_to_project}
-              />
-            ))}
-      </div>
+      <div className={styles.content}>{content}</div>
       <div className={styles.contacts}>
         <Contacts />
         <Request isFromPortfolioPage={true} />
