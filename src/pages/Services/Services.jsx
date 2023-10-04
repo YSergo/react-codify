a71b57c8da719f0a1867c04afecde4f215cd4dbb
YSergo <102493873+YSergo@users.noreply.github.com>
2023-10-04 15:06:59 +0400

    Refactor renderSkeletons in service page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -10,93 +10,59 @@ function Services({ setDrawerOpened, favors, favorsLoading, isMobile, setSelecte
     window.scrollTo(0, 0);
   }, []);
 
-  const renderSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 8; i++) {
-      skeletons.push(
-        <ContentLoader
-          key={i}
-          speed={2}
-          width={446}
-          height={226}
-          viewBox='0 0 446 226'
-          backgroundColor='#f3f3f3'
-          foregroundColor='white'
-        >
-          <rect x='0' y='0' rx='0' ry='0' width='446' height='226' />
-        </ContentLoader>
-      );
-    }
-    return skeletons;
-  };
-
-  const renderMobileSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 8; i++) {
-      skeletons.push(
-        <ContentLoader
-          key={i}
-          speed={2}
-          width='22.5rem'
-          height='12rem'
-          viewBox='0 0 446 226'
-          backgroundColor='#f3f3f3'
-          foregroundColor='white'
-        >
-          <rect x='0' y='0' rx='0' ry='0' width='100%' height='100%' />
-        </ContentLoader>
-      );
-    }
-    return skeletons;
-  };
-
-  const renderTabletSkeletons = () => {
-    const skeletons = [];
-    for (let i = 0; i < 6; i++) {
-      skeletons.push(
-        <ContentLoader
-          key={i}
-          speed={2}
-          width='calc(50% - 2.4vw)'
-          height='23vw'
-          viewBox='0 0 446 226'
-          backgroundColor='#f3f3f3'
-          foregroundColor='white'
-        >
-          <rect x='0' y='0' rx='0' ry='0' width='100%' height='100%' />
-        </ContentLoader>
-      );
-    }
-    return skeletons;
-  };
-
-  const [servicesIsOrigin, setServicesIsOrigin] = useState(true);
-
-  // Обработчик для установки данных из выбранной карточки
+/*   Обработчик для установки данных из выбранной карточки */
   const handleCardClick = (data) => {
     setSelectedCardData(data);
   };
 
+  const renderSkeletons = (count, width, height) => {
+    return Array(count)
+      .fill()
+      .map((_, i) => (
+        <ContentLoader
+          key={i}
+          speed={2}
+          width={width}
+          height={height}
+          viewBox='0 0 446 226'
+          backgroundColor='#f3f3f3'
+          foregroundColor='white'
+        >
+          <rect x='0' y='0' rx='0' ry='0' width='100%' height='100%' />
+        </ContentLoader>
+      ));
+  };
+
+  let content;
+  if (favorsLoading) {
+    switch (true) {
+      case isMobile:
+        content = renderSkeletons(8, '22.5rem', '12rem');
+        break;
+      case isTablet:
+        content = renderSkeletons(6, 'calc(50% - 2.4vw)', '23vw');
+        break;
+      default:
+        content = renderSkeletons(8, 446, 226);
+    }
+  } else {
+    content = favors.map((item) => (
+      <ServiceCard
+        key={item.id}
+        title={item.title}
+        description={item.description}
+        price={item.price}
+        setDrawerOpened={setDrawerOpened}
+        onCardClick={handleCardClick}
+      />
+    ));
+  }
+
+  const [servicesIsOrigin, setServicesIsOrigin] = useState(true);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.content}>
-        {favorsLoading
-          ? isMobile
-            ? renderMobileSkeletons()
-            : isTablet
-            ? renderTabletSkeletons()
-            : renderSkeletons()
-          : favors.map((item) => (
-              <ServiceCard
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                price={item.price}
-                setDrawerOpened={setDrawerOpened}
-                onCardClick={handleCardClick}
-              />
-            ))}
-      </div>
+      <div className={styles.content}>{content}</div>
       <div className={styles.contacts}>
         <Contacts />
         <Request servicesIsOrigin={() => setServicesIsOrigin(servicesIsOrigin)} />
